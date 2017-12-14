275f8e0744ae39fc5d6ae2b0d49a20406dbeb5bb
Benoît Le Gall <benoit.legall@bio-c-bon.fr>
2017-12-14 12:45:20 +0100

    3 columns grid

--- app/src/main/java/fr/frogdevelopment/bibluelle/gallery/GalleryFragment.java
+++ app/src/main/java/fr/frogdevelopment/bibluelle/gallery/GalleryFragment.java
@@ -3,6 +3,7 @@ package fr.frogdevelopment.bibluelle.gallery;
 import android.arch.lifecycle.LiveData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
@@ -46,12 +47,12 @@ public class GalleryFragment extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_gallery, container, false);
 	}
 
 	@Override
-	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
 		mRecyclerView = view.findViewById(R.id.recycler_view);
@@ -62,7 +63,7 @@ public class GalleryFragment extends Fragment {
 			mPreviews = books;
 
 			// default
-			setSimpleList(books);
+			setGridList(books);
 		});
 	}
 
