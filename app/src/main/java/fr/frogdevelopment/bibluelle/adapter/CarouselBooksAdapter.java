b8f53149e7bd8c683fbea40546a116bd736eb03a
b.legall <benoit.legall@bio-c-bon.fr>
2018-05-11 17:25:23 +0200

    improve carousel view

--- app/src/main/java/fr/frogdevelopment/bibluelle/adapter/CarouselBooksAdapter.java
+++ app/src/main/java/fr/frogdevelopment/bibluelle/adapter/CarouselBooksAdapter.java
@@ -1,41 +1,84 @@
 package fr.frogdevelopment.bibluelle.adapter;
 
-import android.databinding.DataBindingUtil;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.util.List;
 
+import fr.frogdevelopment.bibluelle.GlideApp;
 import fr.frogdevelopment.bibluelle.R;
 import fr.frogdevelopment.bibluelle.data.entities.BookPreview;
-import fr.frogdevelopment.bibluelle.databinding.ItemCarouselBinding;
 
-public class CarouselBooksAdapter extends AbstractBooksAdapter<CarouselBooksAdapter.ViewHolder> {
+public class CarouselBooksAdapter extends RecyclerView.Adapter<CarouselBooksAdapter.ViewHolder> {
 
-	static class ViewHolder extends AbstractBooksAdapter.BookViewHolder {
+    private final List<BookPreview> mItems;
+    private final OnClickListener mListener;
 
-		private final ItemCarouselBinding mBinding;
+    public CarouselBooksAdapter(List<BookPreview> previews, OnClickListener listener) {
+        this.mItems = previews;
+        this.mListener = listener;
+    }
 
-		ViewHolder(View itemView) {
-			super(itemView);
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_carousel, parent, false));
+    }
 
-			mBinding = DataBindingUtil.bind(itemView);
-		}
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        BookPreview preview = mItems.get(position);
 
-		@Override
-		protected void bind(BookPreview preview) {
-			mBinding.setPreview(preview);
-		}
-	}
+        viewHolder.itemView.setTag(preview);
+        viewHolder.itemView.setOnClickListener(v -> mListener.onClick(v, preview));
 
-	public CarouselBooksAdapter(List<BookPreview> previews, OnClickListener listener) {
-		super(previews, listener);
-	}
+        Context context = viewHolder.itemView.getContext();
 
-	@Override
-	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_carousel, parent, false));
-	}
+        GlideApp.with(context)
+                .asDrawable()
+                .load(context.getFileStreamPath(preview.getCoverFile()))
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+                .into(new SimpleTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                        RippleDrawable rippledImage = new RippleDrawable(ColorStateList.valueOf(context.getColor(R.color.colorPrimaryDark)), resource, null);
 
+                        viewHolder.mCoverView.setImageDrawable(rippledImage);
+                    }
+                });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(@NonNull ViewHolder holder) {
+        GlideApp.with(holder.itemView.getContext()).clear((holder.mCoverView));
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView mCoverView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            mCoverView = itemView.findViewById(R.id.item_cover);
+        }
+    }
 }
